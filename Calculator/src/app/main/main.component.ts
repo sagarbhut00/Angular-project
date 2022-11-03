09063d15bac318b47221a601a61cfeb5ffd8dcd9
Sagar <sagarbhut2000@gmail.com>
2022-11-03 17:37:57 +0530

    second commit

--- Calculator/src/app/main/main.component.ts
+++ Calculator/src/app/main/main.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  answer = '';
+  input = '';
+  answer = false;
+  opratorArr = ['+', '-', '*', '/', '%'];
 
   constructor() { }
 
@@ -15,21 +17,80 @@ export class MainComponent implements OnInit {
   }
 
   func(val: any) {
-    this.answer += val;
+    if (this.input.length < 15) {
+      if (this.answer && this.input !== '') {
+        this.input = ''
+        this.answer = false;
+      }
+      let lastChar = this.input.charAt(this.input.length - 1);
+      let pos = this.getLastPosOpIndex();
+      if (val === '0') {
+        if (this.opratorArr.includes(lastChar)) {
+          this.input += val;
+        }
+        else if (this.input.charAt(pos + 1) !== '0') {
+          this.input += val;
+        }
+        if (this.input === '') {
+          this.input += val;
+        }
+      } else {
+        if (this.input.charAt(pos + 1) !== '0') {
+          this.input += val;
+        } else {
+          this.input = this.input.replace(/.$/, val);
+        }
+      }
+    }
+  }
+  opratorFunc(val: any) {
+
+    if (this.input !== '') {
+      this.answer = false;
+      let lastChar = this.input.charAt(this.input.length - 1);
+      if (!this.opratorArr.includes(lastChar)) {
+        this.input += val;
+      }
+      else if (this.opratorArr.includes(val) && lastChar !== val) {
+        this.input = this.input.replace(/.$/, val);
+      }
+    }
   }
 
-  ansFunction() {
-    this.answer = eval(this.answer);
+  plusMinus() {
+    let lastChar = this.input.charAt(this.input.length - 1);
+    console.log(lastChar);
+    let temp = lastChar === '+' ? '-' : '+';
+    this.input = this.input.replace(lastChar, temp);
+  }
+
+  equalFunction() {
+    if (!this.answer) {
+      let lastChar = this.input.charAt(this.input.length - 1);
+      if (this.opratorArr.includes(lastChar)) {
+        this.input = this.input.slice(0, -1);
+      }
+      this.input = eval(this.input).toString();
+      this.answer = true;
+    }
   }
 
   clearAll() {
-    this.answer = '';
+    this.input = '';
+    this.answer = false;
   }
 
   clear() {
-    this.answer = this.answer.slice(0, -1);
+    this.input = this.input.slice(0, -1);
   }
 
-
-
+  getLastPosOpIndex() {
+    let pos: number;
+    pos = this.input.lastIndexOf('+');
+    if (this.input.lastIndexOf('-') > pos) pos = this.input.lastIndexOf('-');
+    if (this.input.lastIndexOf('*') > pos) pos = this.input.lastIndexOf('*');
+    if (this.input.lastIndexOf('/') > pos) pos = this.input.lastIndexOf('/');
+    if (this.input.lastIndexOf('%') > pos) pos = this.input.lastIndexOf('%');
+    return pos;
+  }
 }
